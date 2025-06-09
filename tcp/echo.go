e1f2ed1914bcff34a78f8af1c662a5289acca016
Chang Michael <xcchenmichael@gmail.com>
2025-06-08 20:37:13 -0400

    TCP client test with NetAssist

--- tcp/echo.go
+++ tcp/echo.go
@@ -1,9 +1,12 @@
 package tcp
 
 import (
+	"bufio"
 	"context"
+	"go-Redis/lib/logger"
 	"go-Redis/lib/sync/atomic"
 	"go-Redis/lib/sync/wait"
+	"io"
 	"net"
 	"sync"
 	"time"
@@ -25,6 +28,10 @@ type EchoHandler struct {
 	closing    atomic.Boolean
 }
 
+func MakeHandler() *EchoHandler {
+	return &EchoHandler{}
+}
+
 func (handler *EchoHandler) Handle(ctx context.Context, conn net.Conn) {
 	if handler.closing.Get() {
 		_ = conn.Close()
@@ -35,10 +42,33 @@ func (handler *EchoHandler) Handle(ctx context.Context, conn net.Conn) {
 	}
 
 	handler.activeConn.Store(client, struct{}{})
-
+	reader := bufio.NewReader(conn)
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			if err != io.EOF {
+				logger.Info("Connection closed by client")
+				handler.activeConn.Delete(client)
+			} else {
+				logger.Warn(err)
+			}
+			return
+		}
+		// Avoid close by handler
+		client.Waiting.Add(1)
+		b := []byte(msg)
+		_, _ = conn.Write(b)
+		client.Waiting.Done()
+	}
 }
 
 func (handler *EchoHandler) Close() error {
-	//TODO implement me
-	panic("implement me")
+	logger.Info("Handler shutting down")
+	handler.closing.Set(true)
+	handler.activeConn.Range(func(key, value interface{}) bool {
+		client := key.(*EchoClient)
+		_ = client.Conn.Close()
+		return true
+	})
+	return nil
 }
