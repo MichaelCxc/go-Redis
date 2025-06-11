ee921fc9ec3a88ddad2cd8b677ee5102792c2425
Chang Michael <xcchenmichael@gmail.com>
2025-06-10 22:57:24 -0400

    Simple echo DB& Handler implementation & Protocol test log

--- resp/handler/handler.go
+++ resp/handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 	"errors"
+	"go-Redis/database"
 	databaseface "go-Redis/interface/database"
 	"go-Redis/lib/logger"
 	"go-Redis/lib/sync/atomic"
@@ -15,10 +16,22 @@ import (
 	"sync"
 )
 
+var (
+	unknownErrReplyBytes = []byte("-ERR Unknown\r\n")
+)
+
 type RespHandler struct {
 	activeConn sync.Map
 	closing    atomic.Boolean
 	db         databaseface.Database
+}
+
+func MakeHandler() *RespHandler {
+	var db databaseface.Database
+	db = database.NewEchoDatabase()
+	return &RespHandler{
+		db: db,
+	}
 }
 
 func (r *RespHandler) closeClient(client *connection.Connection) {
@@ -36,24 +49,43 @@ func (r *RespHandler) Handle(ctx context.Context, conn net.Conn) {
 	ch := parser.ParseStream(conn)
 
 	for payload := range ch {
-		//1,error from channel
+
 		if payload.Err != nil {
+			//1,error from channel
 			if payload.Err == io.EOF || errors.Is(payload.Err, io.ErrUnexpectedEOF) || strings.Contains(payload.Err.Error(), "use of closed network connection") {
 				r.closeClient(client)
 				logger.Info("Connection closed: " + client.RemoteAddr().String())
 				return
 			}
+			//2.protocol error
+			errReply := reply.MakeErrReply(payload.Err.Error())
+			err := client.Write(errReply.ToBytes())
+			if err != nil {
+				r.closeClient(client)
+				logger.Error("Connection closed: " + client.RemoteAddr().String())
+				return
+			}
+			continue
 		}
-		//2.protocol error
-		errReply := reply.MakeErrReply(payload.Err.Error())
-		err := client.Write(errReply.ToBytes())
-		if err != nil {
-			r.closeClient(client)
-			logger.Error("Connection closed: " + client.RemoteAddr().String())
-			return
+		// exec
+		if payload.Data == nil {
+			continue
 		}
-		continue
+		reply, ok := payload.Data.(*reply.MultiBulkReply)
+		if !ok {
+			logger.Info("Require multi bulk reply")
+			continue
+		}
+		res := r.db.Exec(client, reply.Args)
+
+		if res != nil {
+			_ = client.Write(res.ToBytes())
+		} else {
+			_ = client.Write(unknownErrReplyBytes)
+		}
+
 	}
+
 }
 
 func (r *RespHandler) Close() error {
