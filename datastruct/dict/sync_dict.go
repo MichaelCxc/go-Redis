681982539c514734d0a9533723ff3dc68792842b
Chang Michael <xcchenmichael@gmail.com>
2025-06-13 12:45:30 -0400

    Implement add, delete, update func in dict

---
+++ datastruct/dict/sync_dict.go
@@ -0,0 +1,87 @@
+package dict
+
+import "sync"
+
+type SyncDict struct {
+	m sync.Map
+}
+
+func MakeSyncDict() *SyncDict {
+	return &SyncDict{}
+}
+
+func (dict *SyncDict) Get(key string) (val interface{}, exists bool) {
+	val, ok := dict.m.Load(key)
+	return val, ok
+}
+
+func (dict *SyncDict) Len() int {
+	length := 0
+	dict.m.Range(func(key, value interface{}) bool {
+		length++
+		return true
+	})
+	return length
+}
+
+func (dict *SyncDict) Put(key string, val interface{}) (result int) {
+	_, existed := dict.m.Load(key)
+	dict.m.Store(key, val)
+	if existed {
+		return 0
+	}
+	return 1
+}
+
+func (dict *SyncDict) PutIfAbsent(key string, val interface{}) (result int) {
+
+	_, existed := dict.m.Load(key, val)
+	if existed {
+		return 0
+	}
+	dict.m.Store(key, val)
+	return 1
+}
+
+func (dict *SyncDict) PutIfExists(key string, val interface{}) (result int) {
+	_, existed := dict.m.Load(key, val)
+	if existed {
+		dict.m.Store(key, val)
+		return 1
+	}
+	return 0
+}
+
+func (dict *SyncDict) Remove(key string) (result int) {
+	_, existed := dict.m.Load(key)
+	dict.m.Delete(key)
+	if existed {
+		return 1
+	}
+	return 0
+}
+
+func (dict *SyncDict) ForEach(consumer Consumer) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (dict *SyncDict) Keys() []string {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (dict *SyncDict) RandomKeys(limit int) []string {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (dict *SyncDict) RandomDistinctKeys(limit int) []string {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (dict *SyncDict) clear() {
+	//TODO implement me
+	panic("implement me")
+}
