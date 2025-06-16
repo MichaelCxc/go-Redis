846e5a7d6691b1edac8566a568427d7fb9aca401
Chang Michael <xcchenmichael@gmail.com>
2025-06-15 21:20:24 -0400

    Cluster db

--- cluster/cluster_database.go
+++ cluster/cluster_database.go
@@ -1,7 +1,10 @@
 package cluster
 
 import (
+	"context"
 	pool "github.com/jolestar/go-commons-pool/v2"
+	"go-Redis/config"
+	database2 "go-Redis/database"
 	"go-Redis/interface/database"
 	"go-Redis/interface/resp"
 	"go-Redis/lib/consistenthash"
@@ -16,7 +19,27 @@ type ClusterDatabase struct {
 }
 
 func MakeClusterDatabase() *ClusterDatabase {
+	cluster := &ClusterDatabase{
+		self:           config.Properties.Self,
+		db:             database2.NewStandaloneDatabase(),
+		peerPicker:     consistenthash.NewNodeMap(nil),
+		peerconnection: make(map[string]*pool.ObjectPool),
+	}
 
+	nodes := make([]string, 0, len(config.Properties.Peers)+1)
+	cluster.nodes = nodes
+	for _, peer := range config.Properties.Peers {
+		nodes = append(nodes, peer)
+	}
+	nodes = append(nodes, config.Properties.Self)
+	cluster.peerPicker.AddNode(nodes...)
+	ctx := context.Background()
+	for _, peer := range config.Properties.Peers {
+		pool.NewObjectPoolWithDefaultConfig(ctx, &connectionFactory{
+			Peer: peer,
+		})
+	}
+	return cluster
 }
 
 func (c *ClusterDatabase) Exec(client resp.Connection, args [][]byte) resp.Reply {
