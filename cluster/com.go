0a702df94ebbbf1f1be73bd6caccfb85c5d32eef
Chang Michael <xcchenmichael@gmail.com>
2025-06-16 11:18:28 -0400

    broadcast & relay

--- cluster/com.go
+++ cluster/com.go
@@ -3,7 +3,11 @@ package cluster
 import (
 	"context"
 	"errors"
+	"go-Redis/interface/resp"
+	"go-Redis/lib/utils"
 	"go-Redis/resp/client"
+	"go-Redis/resp/reply"
+	"strconv"
 )
 
 func (cluster *ClusterDatabase) getPeerClient(peer string) (*client.Client, error) {
@@ -28,4 +32,29 @@ func (cluster *ClusterDatabase) returnPeerClient(peer string, c *client.Client) 
 		return errors.New("connection not exist")
 	}
 	return pool.ReturnObject(context.Background(), c)
+}
+
+func (cluster *ClusterDatabase) relay(peer string, c resp.Connection, args [][]byte) resp.Reply {
+	if peer == cluster.self {
+		return cluster.db.Exec(c, args)
+	}
+	peerClient, err := cluster.getPeerClient(peer)
+	if err != nil {
+		return reply.MakeErrReply(err.Error())
+	}
+	defer func() {
+		_ := cluster.returnPeerClient(peer, peerClient)
+	}()
+	peerClient.Send(utils.ToCmdLine("SELECT", strconv.Itoa(c.GetDBIndex())))
+	// other nodes have no idea which db was selected
+	return peerClient.Send(args)
+}
+
+func (cluster *ClusterDatabase) broadcast(c resp.Connection, args [][]byte) map[string]resp.Reply {
+	res := make(map[string]resp.Reply)
+	for _, node := range cluster.nodes {
+		reply := cluster.relay(node, c, args)
+		res[node] = reply
+	}
+	return res
 }
